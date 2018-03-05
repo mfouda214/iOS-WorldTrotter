1fada96fa9a98c604552c013d0a68cf480642333
mfouda214 <m.fouda@robone.net>
2018-03-05 12:45:15 +0000

    Add MapView programmatically

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -7,10 +7,23 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
     
     //Override viewWillAppear(_:) if you need the configuration to be done each time the view controller’s view appears onscreen
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        
+        // Set it as *the* view of this view controller
+        view = mapView
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
