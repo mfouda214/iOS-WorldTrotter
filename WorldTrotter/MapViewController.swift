bba592dd159cb5265837a454d9c481f4255b7080
mfouda214 <m.fouda@robone.net>
2018-03-05 21:55:58 +0000

    Updtae silver challenge

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -9,15 +9,18 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     //Override viewWillAppear(_:) if you need the configuration to be done each time the view controller’s view appears onscreen
     
     var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     
     override func loadView() {
         // Create a map view
         mapView = MKMapView()
+        mapView.delegate = self
+        
         
         // Set it as *the* view of this view controller
         view = mapView
@@ -48,6 +51,8 @@ class MapViewController: UIViewController {
         leadingConstraint.isActive = true
         trailingConstraint.isActive = true
         
+        
+        initLocalizationButton(segmentedControl)
     }
     
     
@@ -68,6 +73,41 @@ class MapViewController: UIViewController {
         default:
             break
         }
+    }
+    
+    func initLocalizationButton(_ anyView: UIView!){
+        let localizationButton = UIButton.init(type: .system)
+        localizationButton.setTitle("Localization", for: .normal)
+        localizationButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(localizationButton)
+        
+        //Constraints
+        
+        let topConstraint = localizationButton.topAnchor.constraint(equalTo:anyView
+            .topAnchor, constant: 32 )
+        let leadingConstraint = localizationButton.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor)
+        let trailingConstraint = localizationButton.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        localizationButton.addTarget(self, action: #selector(MapViewController.showLocalization(sender:)), for: .touchUpInside)
+        
+        
+    }
+    
+    @objc func showLocalization(sender: UIButton!){
+        locationManager.requestWhenInUseAuthorization()//se agrega permiso en info.plist
+        mapView.showsUserLocation = true //fire up the method mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation)
+        
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        //This is a method from MKMapViewDelegate, fires up when the user`s location changes
+        let zoomedInCurrentLocation = MKCoordinateRegionMakeWithDistance(userLocation.coordinate, 500, 500)
+        mapView.setRegion(zoomedInCurrentLocation, animated: true)
     }
 }
 
